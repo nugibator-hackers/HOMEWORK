d44e32d22c5addfc0a878d6acffddb5cc6279f13
nugibator-hackers <79287462+nugibator-hackers@users.noreply.github.com>
2024-01-03 23:13:31 +0300

    Create main.py
    New file

---
+++ main.py
@@ -0,0 +1,47 @@
+# Импорт модуля os из стандартной библиотеки 
+# для взаимодействия с операционной системой.
+import os
+# Считывание переменной окружения REMOTE_JUDGE, чтобы определить, 
+# локально выполняется код или на удалённом сервере.
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+
+# Если код выполняется на локальном компьютере, то...
+if LOCAL:
+    # Класс, описывающий элементы связного списка:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def solution(node, idx):
+    if not idx:
+        return node.next_item
+    head = node
+    for i in range(idx):
+        prev_node, node = node, node.next_item
+    prev_node.next_item = node.next_item
+    return head
+
+
+# Тестирующая функция для проверки решения.
+# Не изменяйте её, она не требует вашего внимания.
+def test():
+    node3 = Node("Задача 4: Обследовать грунт в радиусе 3 м", None)
+    node2 = Node("Задача 3: Измерить температуру атмосферы", node3)
+    node1 = Node("Задача 2: Пробурить скважину глубиной 0.5 м", node2)
+    node0 = Node("Задача 1: Фотосъёмка 360°", node1)
+
+    new_head = solution(node0, 1)
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+    # result is node0 -> node2 -> node3
+    temp_node = node0
+    while temp_node is not None:
+        print(temp_node.value)
+        temp_node = temp_node.next_item
+
+if __name__ == '__main__':
+    test()
