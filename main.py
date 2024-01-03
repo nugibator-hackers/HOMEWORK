a833928466fdf2d18406b2ed6634d41c5eaf2a3f
nugibator-hackers <79287462+nugibator-hackers@users.noreply.github.com>
2024-01-03 23:19:33 +0300

    Update main.py
    FIlr

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-def is_correct_brackets(expr) : 
-    stack = [] 
-    for char in expr: 
-        if char in ["(", "{", "["]: 
-            stack.append(char) 
-        else:  
-            if not stack: 
-                return False
-            current_char = stack.pop() 
-            if current_char == '(': 
-                if char != ")": 
-                    return False
-            if current_char == '{': 
-                if char != "}": 
-                    return False
-            if current_char == '[': 
-                if char != "]": 
-                    return False
-    if stack: 
-        return False
-    return True
-
-expr = input()
-print(is_correct_brackets(expr))
-if __name__ == '__main__':
-    is_correct_brackets(expr)
+# 104042731
+def mars_roverars(s) -> int: 
+    """Рассчитать количество платформ.""" 
+    if not s:
+        return 0
+    max_str = 0
+    l = 0 
+    uniq = set()
+    for r in range(len(s)):
+        while s[r] in uniq:
+            uniq.remove(s[l])
+            l += 1
+        uniq.add(s[r])
+        max_str = max(max_str, r-l+1)
+    return max_str        
+ 
+def main() -> None: 
+    s = str(input())  
+    print(mars_roverars(s)) 
+ 
+if __name__ == '__main__': 
+    main()
