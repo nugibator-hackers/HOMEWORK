6500e83817abbf650602dd8e4a3e355c29280c4b
nugibator-hackers <79287462+nugibator-hackers@users.noreply.github.com>
2024-01-03 23:54:45 +0300

    Update main.py
    KK

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-def joseph(n, arr1, k, arr2) -> int:
+def joseph(n, arr) -> int:
     """Рассчитать количество платформ."""
-    left = 0
-    
-    for i in range(k):
-        for j in range(left, n):
-            if arr2[i] == arr1[j]:
-                arr1[left], arr1[j] = arr1[j], arr1[left]
-                left += 1  
-    for i in range(left, len(arr1)):
-        for j in range(i, left, -1):
-            if arr1[j] < arr1[j-1]:
-                arr1[j], arr1[j-1] = arr1[j-1], arr1[j]
-            else:
-                break
-    return arr1
+    k = 0
+    sum = 0
+    count = 0
+    for i in arr:
+        sum += i
+        k += 1
+        if sum == (k * k - k) / 2:
+                count += 1      
+    return count
  
 def main() -> None:
     n = int(input())
-    arr1: list = [int(array_counter) for array_counter in input().split(' ')]
-    k = int(input())
-    if k == 0:
-        print(*joseph(n, arr1, k, []))
-    else:
-        arr2: list = [int(array_counter) for array_counter in input().split(' ')]
-        print(*joseph(n, arr1, k, arr2))
+    arr: list = [int(array_counter) for array_counter in input().split(' ')]
+    print(joseph(n, arr))
  
 if __name__ == '__main__': 
     main()
